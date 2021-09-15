3441a92aa39ec9b6e959dc89e659997c1064645b
dhaniemubarak <dhaniemubarak@gmail.com>
2021-09-15 12:45:28 +0700

    HOL-14 Inisiasi project

---
+++ src/main/java/id/holigo/services/holigouserservice/HoligoUserServiceApplication.java
@@ -0,0 +1,13 @@
+package id.holigo.services.holigouserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoligoUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoligoUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/holigo/services/holigouserservice/HoligoUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.holigo.services.holigouserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoligoUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
